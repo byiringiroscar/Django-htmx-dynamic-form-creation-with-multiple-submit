ca2d4f79c71a65122ed6f3689e7b14155bfc28f7
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-09 09:41:37 +0200

    Add forms

---
+++ core/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'phone_number')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+        }
