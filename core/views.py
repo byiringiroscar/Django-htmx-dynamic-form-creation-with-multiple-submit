e644f08bc089d0502235cda70f1ec7d4699f6508
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-09 10:05:18 +0200

    wip on form

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from core.forms import ContactForm
+from core.models import Contact
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'form': ContactForm(),
+    }
+    return render(request, 'index.html', context)
